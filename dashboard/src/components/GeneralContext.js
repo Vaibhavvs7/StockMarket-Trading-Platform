f8bb2cefd9a4f97797623facd89625534fe5e620
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-07-12 22:44:35 +0530

    Fix: removed nested git repos from backend and dashboard

---
+++ dashboard/src/components/GeneralContext.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import BuyActionWindow from "./BuyActionWindow";
+import SellActionWindow from "./SellActionWindow";
+
+// Single GeneralContext
+const GeneralContext = React.createContext({
+  openBuyWindow: (uid) => {},
+  closeBuyWindow: () => {},
+  openSellWindow: (uid) => {},
+  closeSellWindow: () => {},
+});
+
+export const GeneralContextProvider = (props) => {
+  const [isBuyWindowOpen, setIsBuyWindowOpen] = useState(false);
+  const [isSellWindowOpen, setIsSellWindowOpen] = useState(false);
+  const [selectedStockUID, setSelectedStockUID] = useState("");
+
+  const handleOpenBuyWindow = (uid) => {
+    setIsBuyWindowOpen(true);
+    setSelectedStockUID(uid);
+  };
+
+  const handleCloseBuyWindow = () => {
+    setIsBuyWindowOpen(false);
+    setSelectedStockUID("");
+  };
+
+  const handleOpenSellWindow = (uid) => {
+    setIsSellWindowOpen(true);
+    setSelectedStockUID(uid);
+  };
+
+  const handleCloseSellWindow = () => {
+    setIsSellWindowOpen(false);
+    setSelectedStockUID("");
+  };
+
+  return (
+    <GeneralContext.Provider
+      value={{
+        openBuyWindow: handleOpenBuyWindow,
+        closeBuyWindow: handleCloseBuyWindow,
+        openSellWindow: handleOpenSellWindow,
+        closeSellWindow: handleCloseSellWindow,
+      }}
+    >
+      {props.children}
+      {/* Conditional rendering of Buy and Sell Action Windows */}
+      {isBuyWindowOpen && <BuyActionWindow uid={selectedStockUID} />}
+      {isSellWindowOpen && <SellActionWindow uid={selectedStockUID} />}
+    </GeneralContext.Provider>
+  );
+};
+
+export default GeneralContext;
